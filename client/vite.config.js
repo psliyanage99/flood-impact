732ae98832bb5cd8655743c41dea6ea3e60afb4b
Praneeth S liyanage <praneethsbliyanage@gmail.com>
2026-02-04 20:53:13 +0530

    Changed API

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import Sitemap from 'vite-plugin-sitemap' // 1. Import the plugin
+import Sitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    // 2. Add the Sitemap configuration here
     Sitemap({ 
       hostname: 'https://floodimpact.online',
       dynamicRoutes: ['/dashboard', '/admin', '/report'] 
     }),
   ],
+  // ADD THIS SECTION FOR LOCAL DEVELOPMENT
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://13.60.179.95:8081',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
